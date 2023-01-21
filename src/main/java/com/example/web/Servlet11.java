a66bb5599ff6ef3272e4edadf7ef579a8cfe5854
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-21 20:57:17 +0100

    add first servlet

---
+++ src/main/java/com/example/web/Servlet11.java
@@ -0,0 +1,21 @@
+package com.example.web;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Servlet11", urlPatterns = "/servlet11")
+public class Servlet11 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        System.out.println("Hello first servlet");
+        response.getWriter().append("Content11\n");
+        response.getWriter().append("SERVLET WORKS !!!");
+    }
+}
