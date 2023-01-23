046d4986a45e1814440b6c8b9b05c3af434c12fa
R0dr1g3z <r0dr1g3zek@gmail.com>
2023-01-23 08:35:57 +0100

    format documents

--- src/main/java/com/example/web/Servlet11.java
+++ src/main/java/com/example/web/Servlet11.java
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "Servlet11", urlPatterns = "/servlet11")
 public class Servlet11 extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("Hello first servlet");
         response.getWriter().append("Content11\n");
         response.getWriter().append("SERVLET WORKS !!!");
--- src/main/java/com/example/web/Servlet13.java
+++ src/main/java/com/example/web/Servlet13.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/servlet13")
 public class Servlet13 extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.getWriter().println(LocalTime.now());
     }
 }
